b29f9023b1c1c10a8ee6b2a56521e2d29a98d447
Christian Iversen <ci@iversenit.dk>
2015-12-18 00:31:11 +0100

    + Added cheaty version

---
+++ xmas2.py
@@ -0,0 +1,4 @@
+#encoding=utf-8
+print file(__file__).read().split("\n##")[1].replace("\n#", "\n").decode("zlib")
+##xÚå’IN1E÷}Š:Ìì MÂ"ˆô¬¸H—ð•Ý	¹=v‹=2b+[¶üë½/W±#Ô!B«=üÆpˆV…Êî¥'¿%rÑÃÒUu…’(¬×vPX´BTUo#§ÛGÛK4„&Ý…J¥çêý)ÇR4¥Ko1r«wäÂ‡çžÄ®`îÄ÷‚±2†Ý7,:|"Êi
+#§ƒ©7šNøZ¸³"¸%¿`J(eÍÔ>}7ïy/mÖr§\HiË[›™^ã¢Hã–×]Ä\±Îs6ÏƒÅo•»,’»gG	T34Ê­2ð®ŠŒÛD5ó2¸ÍHm2ñ!´P—}j3ˆ,xCò’‘µšQQ5õôÖæb†Í üúz€0	Æ
