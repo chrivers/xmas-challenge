d4dd44c1e0008692b28438d57e428e043f91b768
Christian Iversen <ci@iversenit.dk>
2015-12-21 00:42:27 +0100

    + Improved size statistics

--- size.py
+++ size.py
@@ -2,11 +2,11 @@
 import sys
 filename = sys.argv[1]
 text = file(filename).read()
-text = text \
+text2 = text \
        .replace(" ", "") \
        .replace("\t", "") \
        .replace("\n", "") \
        .replace("\r", "") \
        .replace("\f", "") \
        .replace("\v", "")
-print "%s: %3d characters" % (filename, len(text))
+print "%s: %6d total, %6d whitespace, %6d non-whitespace" % (filename, len(text), len(text)-len(text2), len(text2))
