dc58d0c95a0082d207c1d094f28ecfd02c7e67c9
Christian Iversen <ci@iversenit.dk>
2015-12-22 12:04:37 +0100

    + Implemented initial whitespace assembler

--- whitespace/__init__.py
+++ whitespace/__init__.py
@@ -1,4 +1,5 @@
 import re
+from StringIO import StringIO
 
 re_comment = re.compile("\s*;.*")
 re_push    = re.compile("\s*(PUSH)\s+(\d+)\s*")
@@ -25,9 +26,39 @@ class WhitespaceAssemblerParser(object):
                 raise ValueError("Parse error at line %d: unknown line [%r]" % (index, line))
         return result
 
+class WhitespaceAssembler(object):
+
+    TOKENS_1OP = ["dup", "swap", "pop", "add", "sub", "mul", "div", "mod", "get", "set", "ret", "halt", "write", "out", "read", "in"]
+    TOKENS_2OP = ["call", "jump", "jumpz", "jumpn"]
+
+    def __init__(self):
+        super(WhitespaceAssembler, self).__init__()
+        self.buf = StringIO()
+
+    def assemble(self, tokens):
+        for token in tokens:
+            name = token[0].lower()
+            if name in self.TOKENS_1OP:
+                if hasattr(self, name):
+                    getattr(self, name)()
+                else:
+                    raise ValueError("Unknown operation %s!" % name)
+            elif name in self.TOKENS_2OP:
+                if hasattr(self, name):
+                    getattr(self, name)(tokens[1])
+                else:
+                    raise ValueError("Unknown operation %s %s!" % (name, tokens[1]))
+            elif name == "push":
+                self.push(tokens[1])
+            elif name[-1] == ":":
+                self.label(name[:-1])
+            else:
+                raise ValueError("Unknown operation %s!" % name)
+
 if __name__ == "__main__":
     import sys
-    wap = WhitespaceAssemblerParser()
-    res = wap.parse(sys.stdin.read())
     from pprint import pprint
-    pprint(res)
+    wap = WhitespaceAssemblerParser()
+    tokens = wap.parse(sys.stdin.read())
+    wa = WhitespaceAssembler()
+    pprint(wa.assemble(tokens))
