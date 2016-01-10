bdc5c02d825b2a211b45af025ae4ef253b33e63b
Christian Iversen <ci@iversenit.dk>
2016-01-10 21:24:35 +0100

    * Refactored shared code into xmas5py module

--- encoder.py
+++ encoder.py
@@ -1,26 +1,11 @@
 #!/usr/bin/python
 
 import sys
-
-def total_bit_width(string, offset):
-    bits = 0
-    for c in string:
-        bits += len(bin(abs(ord(c) - offset))[2:].lstrip("0"))
-    return bits
-
-def best_offset(string):
-    best = 0xFFFFFFFF
-    index = None
-    for x in range(0, 256):
-        new = total_bit_width(string, x)
-        if new < best:
-            best = new
-            index = x
-    return index
+import xmas5py
 
 def code(arg):
     print "  ; \"%s\"" % arg.replace("\n", "\\n")
-    best = best_offset(arg)
+    best = xmas5py.best_offset(arg)
     if len(arg) > 3:
         for x in reversed(arg):
             print "  push %d" % (ord(x) - best)
